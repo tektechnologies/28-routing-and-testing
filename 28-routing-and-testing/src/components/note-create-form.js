28e4928edf4d821a3b4d7fa8617471f97c1184e6
tektechnologies <info@tektechnologies.com>
2018-09-19 16:23:19 -0500

    add set up for noteform

---
+++ 28-routing-and-testing/src/components/note-create-form.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+const defaultState = {
+    title: '',
+    amount: 0,
+};
+
+export default class NoteForm extends Component{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//close class
